5ca7acff6e649d0905ead05c799c5736b2af10a2
Drew Sommer <andrew.p.sommer@gmail.com>
2019-09-20 17:40:20 -0700

    feat: Build a simple square root calculator

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Problems vs. Algorithms
 
-Basic algorithms of computer science are explored using Python. The solution to seven problems are discussed with respect to their data structures and time/space complexity, created as part of the [Data Structures and Algorithms Nanodegree by Udacity](https://www.udacity.com/course/data-structures-and-algorithms-nanodegree--nd256).
+The basic algorithms of computer science are explored using Python. Solution to seven problems are discussed with respect to data structures, time and space complexity, created as part of the [Data Structures and Algorithms Nanodegree by Udacity](https://www.udacity.com/course/data-structures-and-algorithms-nanodegree--nd256).
--- problem_1.py
+++ problem_1.py
@@ -7,10 +7,53 @@ def sqrt(number):
     Returns:
        int: Floored Square Root
     """
-    pass
 
-print ("Pass" if  (3 == sqrt(9)) else "Fail")
-print ("Pass" if  (0 == sqrt(0)) else "Fail")
-print ("Pass" if  (4 == sqrt(16)) else "Fail")
-print ("Pass" if  (1 == sqrt(1)) else "Fail")
-print ("Pass" if  (5 == sqrt(27)) else "Fail")
+    # type check input to ensure it it integer
+    if not isinstance(number, int):
+        print("Input must be an integer.")
+        return
+
+    # trivial case, sqrt(0) = 0
+    if number == 0:
+        return 0
+
+    # trivial case, sqrt(1) = 1
+    if number == 1:
+        return 1
+
+    # loop until the square root (floor) is found
+    i = 2
+    while True:
+
+        # compute the square of the counter
+        square = i*i
+
+        # an exact match is found, return the counter
+        if square == number:
+            return i
+
+        # the passed integer was exceeded, return the previous integer counter
+        if square > number:
+            return i-1
+
+        # increment the counter
+        i += 1
+
+########## TESTING
+
+print("Pass" if (3 == sqrt(9)) else "Fail")
+# Pass
+print("Pass" if (0 == sqrt(0)) else "Fail")
+# Pass
+print("Pass" if (4 == sqrt(16)) else "Fail")
+# Pass
+print("Pass" if (1 == sqrt(1)) else "Fail")
+# Pass
+print("Pass" if (5 == sqrt(27)) else "Fail")
+# Pass
+sqrt(None)
+# Input must be an integer.
+sqrt("apples")
+# Input must be an integer.
+print(sqrt(42))
+# 6
