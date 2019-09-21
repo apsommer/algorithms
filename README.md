d2c6427c4a45d7306118866af2f5d85e1343458b
Drew Sommer <andrew.p.sommer@gmail.com>
2019-09-21 09:23:23 -0700

    feat: Build a square root calculator using recursion

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Problems vs. Algorithms
 
-The basic algorithms of computer science are explored using Python. Solution to seven problems are discussed with respect to data structures, time and space complexity, created as part of the [Data Structures and Algorithms Nanodegree by Udacity](https://www.udacity.com/course/data-structures-and-algorithms-nanodegree--nd256).
+The basic algorithms of computer science are explored using Python. Seven problems are solved with careful attention to data structures, time complexity, and space complexity, created as part of the [Data Structures and Algorithms Nanodegree by Udacity](https://www.udacity.com/course/data-structures-and-algorithms-nanodegree--nd256).
--- problem_1.py
+++ problem_1.py
@@ -1,6 +1,6 @@
 def sqrt(number):
     """
-    Calculate the floored square root of a number
+    Calculate the floored square root of a number ... using binary search recursion
 
     Args:
        number(int): Number to find the floored squared root
@@ -8,36 +8,44 @@ def sqrt(number):
        int: Floored Square Root
     """
 
-    # type check input to ensure it it integer
+    # type check input to ensure it is integer
     if not isinstance(number, int):
         print("Input must be an integer.")
         return
 
-    # trivial case, sqrt(0) = 0
-    if number == 0:
-        return 0
+    # catch trivial cases
+    if number == 0 or number == 1:
+        return number
 
-    # trivial case, sqrt(1) = 1
-    if number == 1:
-        return 1
+    # begin recursion over one half the input span, since a square root
+    # is never larger than n/2 for n > 2
+    return sqrt_recursion(number, 0, number//2)
 
-    # loop until the square root (floor) is found
-    i = 2
-    while True:
+# recursive algrothim using binary search to achieve the required O(log(n)) time
+def sqrt_recursion(number, start, end):
 
-        # compute the square of the counter
-        square = i*i
+    # calculate a middle number (floor) and its square
+    mid = (end + start)//2
+    square = mid*mid
 
-        # an exact match is found, return the counter
-        if square == number:
-            return i
+    # exact match found this is a perfect square
+    if square == number:
+        return mid
 
-        # the passed integer was exceeded, return the previous integer counter
-        if square > number:
-            return i-1
+    # the true root must be in the upper half of remaining numbers, between mid:end
+    if square < number:
 
-        # increment the counter
-        i += 1
+        # we have recursed as far as possible, since we want the floor value of sqrt(n) the current
+        # value of mid is the correct output
+        if mid == end - 1:
+            return mid
+
+        # recurse into upper half between mid:end
+        return sqrt_recursion(number, mid, end)
+
+    # the true root must be in the lower half of remaining numbers, between start:mid
+    if square > number:
+        return sqrt_recursion(number, start, mid)
 
 ########## TESTING
 
@@ -57,3 +65,48 @@ sqrt("apples")
 # Input must be an integer.
 print(sqrt(42))
 # 6
+
+########## ALTERNATIVE SOLUTION
+
+# the following implementation requires O(sqrt(n)) time, too slow based on the stated requirements
+# it shows an alternative, iterative, brute force method
+def sqrt_iterative(number):
+    """
+    Calculate the floored square root of a number ... using brute force iteration
+
+    Args:
+       number(int): Number to find the floored squared root
+    Returns:
+       int: Floored Square Root
+    """
+
+    # type check input to ensure it is integer
+    if not isinstance(number, int):
+        print("Input must be an integer.")
+        return
+
+    # trivial case, sqrt(0) = 0
+    if number == 0:
+        return 0
+
+    # trivial case, sqrt(1) = 1
+    if number == 1:
+        return 1
+
+    # loop until the square root (integer floor) is found
+    i = 2
+    while True:
+
+        # compute the square of the counter
+        square = i*i
+
+        # an exact match is found, return the counter
+        if square == number:
+            return i
+
+        # the passed integer was exceeded, return the previous integer counter
+        if square > number:
+            return i-1
+
+        # increment the counter
+        i += 1
