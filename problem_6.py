7b88ac854e70392d22396bce07afff25edaf5a44
Drew Sommer <andrew.p.sommer@gmail.com>
2019-09-22 18:53:07 -0700

    feat: Find min and max of an unsorted array in a single traversal

--- problem_6.py
+++ problem_6.py
@@ -5,12 +5,48 @@ def get_min_max(ints):
     Args:
        ints(list): list of integers containing one or more integers
     """
-   pass
 
-## Example Test Case of Ten Integers
+    # catch bad input: less than one element in passed list
+    if len(ints) < 1:
+        print("Input list must contain at least one integer.")
+        return
+
+    # catch bad input: elements out of range
+    for element in ints:
+        if not isinstance(element, int) or element < 0 or element > 9:
+            print("Input must be a list of integers i, where 0 < i < 9.")
+            return
+
+    # step through the array and capture the smallest and largest elements using simple comparison
+
+    # start the min and max storage variables at the first element
+    min = ints[0]
+    max = ints[0]
+
+    # loop over the input list
+    for element in ints:
+
+        # current element is less than min, up min
+        if element < min:
+            min = element
+
+        # current element is greater than max, update max
+        if element > max:
+            max = element
+
+    return (min, max)
+
+########## TESTING
 import random
-
-l = [i for i in range(0, 10)]  # a list containing 0 - 9
+l = [i for i in range(0, 10)]
 random.shuffle(l)
-
-print ("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
+print("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
+# Pass
+print("Pass" if ((0, 5) == get_min_max([3, 0, 1, 2, 4, 5, 3])) else "Fail")
+# Pass
+print("Pass" if ((4, 4) == get_min_max([4])) else "Fail")
+# Pass
+get_min_max([])
+# Input list must contain at least one integer.
+get_min_max(["apples"])
+# Input must be a list of integers i, where 0 < i < 9.
