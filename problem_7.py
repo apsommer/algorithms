35caa6ac2aa6ac8ec28acbfdddb7b8367f21d6e6
Drew Sommer <andrew.p.sommer@gmail.com>
2019-09-25 16:07:54 -0700

    feat: Build router class to wrap trie for simplified http routing

--- problem_7.py
+++ problem_7.py
@@ -6,70 +6,133 @@ class RouteTrieNode:
         self.children = {}
         self.handler = handler
 
-    # add a child, where all children are hashmap pairs char:node
-    def insert(self, char):
-        self.children[char] = RouteTrieNode()
+    # add a child node
+    def insert(self, endpoint):
+        self.children[endpoint] = RouteTrieNode()
 
 # stores routes and their associated handlers
 class RouteTrie:
 
     # constructor: initialize the trie with a root node, this is the root path or homepage node
-    def __init__(self):
-        self.root = RouteTrieNode()
+    def __init__(self, handler = None):
+        self.root = RouteTrieNode(handler)
 
-    # Similar to our previous example you will want to recursively add nodes
-    # Make sure you assign the handler to only the leaf (deepest) node of this path
-    def insert(self, path):
+    # recursively add nodes to the trie
+    def insert(self, endpoints, handler):
 
+        # recursively add nodes to the trie
+        def _insert(node, i):
+
+            # get this segment of the path
+            endpoint = endpoints[i]
+
+            # if the endpoint does not exist then create a node for it
+            if endpoint not in node.children:
+                node.insert(endpoint)
+
+            # move to the next node and next endpoint
+            node = node.children[endpoint]
+
+            # handler is assigned only to the leaf (deepest) node of this path
+            if i == len(endpoints) - 1 :
+                node.handler = handler
+                return
+
+            _insert(node, i + 1)
+
+        # start recursion from the root and the first endpoint in the full path string
+        _insert(self.root, 0)
+
+    # iteratively find node in the trie
+    def find(self, endpoints):
+
+        # start at the root
         node = self.root
 
-        def _insert(node, path):
+        # starting at the root, navigate the trie to find a match for this path endpoint
+        for endpoint in endpoints:
 
-            
+            # move to next node
+            node = node.children.get(endpoint, None)
 
-        _insert(node, path)
+            # if the node does not exist in the trie return none
+            if node == None:
+                return None
 
-    def find(self, ...):
-        # Starting at the root, navigate the Trie to find a match for this path
-        # Return the handler for a match, or None for no match
+        # node found, return its handler string
+        return node.handler
 
-
-
-# The Router class will wrap the Trie and handler
+# the router class wraps the trie
 class Router:
-    def __init__(self, ...):
-        # Create a new RouteTrie for holding our routes
-        # You could also add a handler for 404 page not found responses as well!
 
-    def add_handler(self, ...):
-        # Add a handler for a path
-        # You will need to split the path and pass the pass parts
-        # as a list to the RouteTrie
+    # constructor: create a new trie to hold the routes and a "not found" handler
+    def __init__(self, root_handler = None, error_handler = "Error 404 page not found."):
+        self.trie = RouteTrie(root_handler)
+        self.error_handler = error_handler
 
-    def lookup(self, ...):
-        # lookup path (by parts) and return the associated handler
-        # you can return None if it's not found or
-        # return the "not found" handler if you added one
+    # add handler to a path endpoint
+    def add_handler(self, path, handler):
+
+        # catch malformed path
+        if not isinstance(path, str) or path[0] != '/':
+            print("Invalid path.")
+            return
+
+        # catch malformed handler
+        if not isinstance(handler, str):
+            print("Invalid handler.")
+            return
+
+        # split the path and pass the parts as list to the trie
+        endpoints = self.split_path(path)
+        self.trie.insert(endpoints, handler)
+
+    # return the handler for a given path
+    def lookup(self, path):
         # bonus points if a path works with and without a trailing slash
         # e.g. /about and /about/ both return the /about handler
 
+        # edge case: catch the root
+        if path == "/" or "":
+            return self.trie.root.handler
 
-    def split_path(self, ...):
-        # you need to split the path into parts for
-        # both the add_handler and loopup functions,
-        # so it should be placed in a function here
+        # split the path and pass the parts as list to the trie
+        endpoints = self.split_path(path)
+        handler = self.trie.find(endpoints)
 
-#####################################
+        # if the endpoint handler was not found return an error message
+        if handler == None:
+            return self.error_handler
 
-# Here are some test cases and expected outputs you can use to test your implementation
+        return handler
 
-# create the router and add a route
-router = Router("root handler", "not found handler") # remove the 'not found handler' if you did not implement this
-router.add_handler("/home/about", "about handler")  # add a route
+    # split path string to a list around the "/" delimiter
+    def split_path(self, path):
 
-# some lookups with the expected output
-print(router.lookup("/")) # should print 'root handler'
-print(router.lookup("/home")) # should print 'not found handler' or None if you did not implement one
-print(router.lookup("/home/about")) # should print 'about handler'
-print(router.lookup("/home/about/")) # should print 'about handler' or None if you did not handle trailing slashes
-print(router.lookup("/home/about/me")) # should print 'not found handler' or None if you did not implement one
+        # split path string to a list around the "/" delimiter
+        endpoints = path.split("/")
+
+        # preceeding and ending "/" cause blank elements, trim them off
+        endpoints = endpoints[1:]
+        if endpoints[-1] == '':
+            endpoints = endpoints[:-1]
+
+        return endpoints
+
+########## TESTING
+router = Router("root handler", "not found handler")
+router.add_handler("/home/about", "about handler")
+print(router.lookup("/"))
+# root handler
+print(router.lookup("/home"))
+# not found handler
+print(router.lookup("/home/about"))
+# about handler
+print(router.lookup("/home/about/"))
+# about handler
+print(router.lookup("/home/about/me"))
+# not found handler
+router.add_handler("bad_path", "handler")
+# Invalid path.
+router.add_handler("/apples", 123)
+# Invalid handler.
